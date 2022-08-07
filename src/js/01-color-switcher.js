e52d7920001f31a00c668e1213c0436ff00605ba
Sławomir Albiński <slawomiralbinski@gmail.com>
2022-08-07 22:27:15 +0200

    z

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let bgChange = null;
+
+const colorChanger = () => {
+  return (body.style.backgroundColor = `${getRandomHexColor()}`);
+};
+
+const colorStop = () => {
+  clearInterval(bgChange);
+  btnStart.disabled = false;
+};
+
+btnStart.addEventListener('click', () => {
+  bgChange = setInterval(colorChanger, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', colorStop);
