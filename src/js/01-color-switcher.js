96d02a8a06dafa5ec91394d11d1a4cfe43da0293
Sławomir Albiński <slawomiralbinski@gmail.com>
2022-08-11 23:08:27 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,4 @@
-body = document.querySelector('body');
+const body = document.querySelector('body');
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 
