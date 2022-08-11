b6089e8818d356dd4bdf5dbdc3f4f8e4a2669b24
Drwalbin <99756243+Drwalbin@users.noreply.github.com>
2022-08-11 21:05:55 +0000

    Deploying to gh-pages from @ Drwalbin/goit-js-hw-09@2c69a4b076a29f464f37fb88b80382b32ef2cbc6 🚀

---
+++ 01-color-switcher.16cc6596.js
@@ -0,0 +1,2 @@
+!function(){body=document.querySelector("body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");var n=null,o=function(){return body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))};t.addEventListener("click",(function(){n=setInterval(o,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.16cc6596.js.map
