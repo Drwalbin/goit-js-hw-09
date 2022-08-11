68cab9906e975bfa5b6a986adbf70b3973da6ae3
Sławomir Albiński <slawomiralbinski@gmail.com>
2022-08-11 22:57:13 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,6 @@
-
 const body = document.querySelector('body');
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
